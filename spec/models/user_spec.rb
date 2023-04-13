fb48b14b1cb0d2186112bb2f897ddf2f9580c0be
mpg-asahi-fuke <asahi.fuke@medpeer.co.jp>
2023-04-13 12:39:38 +0900

    テストです。

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "ok" do
-    expect(1+1).to eq 2
+  it "１＋１は２である" do
+    expect(1 + 1).to eq 2
+  end
+
+  it "1+2は3である" do
+    expect(1 + 2).to eq 3
   end
 end
